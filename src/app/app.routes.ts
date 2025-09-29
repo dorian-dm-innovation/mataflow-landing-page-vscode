467723aa0b16562c43cb8a72249d69540b7ba710
dorian-dm-innovation <166225125+dorian-dm-innovation@users.noreply.github.com>
2025-09-29 20:40:41 +0200

    Updated app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
