9fd884239bb0d499e0e21b40e8900fc2677e3f98
dorian-dm-innovation <166225125+dorian-dm-innovation@users.noreply.github.com>
2025-10-01 13:53:21 +0200

    Updated partners-section.component.ts

--- src/app/components/parteners-section/partners-section.component.ts
+++ src/app/components/parteners-section/partners-section.component.ts
@@ -10,6 +10,7 @@ import { Partner } from '../../models/partner.model';
 })
 export class PartnersSectionComponent {
   isContactModalOpen = false;
+  isSubmitting= false;
   partners: Partner[] = [
     { name: "Partenaire 1", logo: "/partners/partner-1-logo.png", alt: "Logo Partenaire 1" },
     { name: "Partenaire 2", logo: "/partners/partner-2-logo.png", alt: "Logo Partenaire 2" },
